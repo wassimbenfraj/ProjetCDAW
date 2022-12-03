5f9fdf13fec61cdf1cfdd27db850c44433fcaae0
wassimbenfraj <wassim.benfraj@esprit.tn>
2022-12-03 14:25:51 +0100

    Added Configuration

---
+++ PokemonProject/resources/views/helloworld.blade.php
@@ -0,0 +1,12 @@
+<h1>Hello World</h1>
+<h1>{{ $title }}</h1>
+
+@if ($errors->any())
+    <div class="alert alert-danger">
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
