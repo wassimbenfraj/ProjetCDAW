0efbba94a84cef21148078a4786480a6cbd54474
unknown <laila.allam.001@gmail.com>
2022-12-05 12:05:05 +0100

    adding sign up page

---
+++ PokemonProject/resources/views/signup.blade.php
@@ -0,0 +1,69 @@
+@extends('layouts.app')
+
+@section('title', 'Sign Up')
+
+
+@section('content')
+
+<!-- start page title area -->
+<div class="rn-breadcrumb-inner ptb--30">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-lg-6 col-md-6 col-12">
+                    <h5 class="title text-center text-md-start">Sign Up</h5>
+                </div>
+                <div class="col-lg-6 col-md-6 col-12">
+                    <ul class="breadcrumb-list">
+                        <li class="item"><a href="index.html">Acceuil</a></li>
+                        <li class="separator"><i class="feather-chevron-right"></i></li>
+                        <li class="item current">Sign Up</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- end page title area -->
+
+    <!-- login form -->
+    <div class="login-area rn-section-gapTop">
+        <div class="container">
+            <div class="row g-5">
+                <div class="offset-2 col-lg-4 col-md-6 ml_md--0 ml_sm--0 col-sm-12">
+                    <div class="form-wrapper-one registration-area">
+                        <h4>Sign up</h4>
+                        <form>
+                            <div class="mb-5">
+                                <label for="firstName" class="form-label">Pseudo</label>
+                                <input type="text" id="firstName">
+                            </div>
+                            <div class="mb-5">
+                                <label for="sastName" class="form-label">Prénom</label>
+                                <input type="email" id="sastName">
+                            </div>
+                            <div class="mb-5">
+                                <label for="sastName" class="form-label">Nom</label>
+                                <input type="email" id="sastName">
+                            </div>
+                            <div class="mb-5">
+                                <label for="exampleInputEmail1" class="form-label">Adresse e-mail</label>
+                                <input type="email" id="exampleInputEmail1">
+                            </div>
+                            <div class="mb-5">
+                                <label for="newPassword" class="form-label">Mot de passe</label>
+                                <input type="password" id="newPassword">
+                            </div>
+                            <div class="mb-5">
+                                <label for="exampleInputPassword1" class="form-label">Confirmer le mot de passe</label>
+                                <input type="password" id="exampleInputPassword1">
+                            </div>
+                            <button type="submit" class="btn btn-primary mr--15">Sign Up</button>
+                            <a href="login.html" class="btn btn-primary-alta">Log In</a>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- login form end -->
+
+@endsection
