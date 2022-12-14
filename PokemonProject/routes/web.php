8da7f3f38b33ee873efb41c968e271e634740e84
wassimbenfraj <wassim.benfraj@esprit.tn>
2022-12-14 17:27:46 +0100

    Testing Protected Routes

--- PokemonProject/routes/web.php
+++ PokemonProject/routes/web.php
@@ -17,14 +17,14 @@ use App\Http\Controllers\Controller;
 
 // Pokemon Controller
 Route::get('/',[\App\Http\Controllers\PokemonController::class, 'index']);
-Route::get('/pokemons',[\App\Http\Controllers\PokemonController::class, 'show']);
+Route::get('/pokemons',[\App\Http\Controllers\PokemonController::class, 'show'])->middleware('auth');
 Route::post('/pokemons/sort',[\App\Http\Controllers\PokemonController::class, 'sortPokemons']);
 
 
 //              User Controller
 
 //login form
-Route::get('/login',[\App\Http\Controllers\UserController::class, 'login']);
+Route::get('/login',[\App\Http\Controllers\UserController::class, 'login'])->name('login');
 //signup form
 Route::get('/signup',[\App\Http\Controllers\UserController::class, 'signup']);
 //register user
