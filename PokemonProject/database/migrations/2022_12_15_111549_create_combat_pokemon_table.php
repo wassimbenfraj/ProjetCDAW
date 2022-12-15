0e9c1013ce55eecd3bd8d6b20e9c695527018acd
wassimbenfraj <wassim.benfraj@esprit.tn>
2022-12-15 12:27:14 +0100

    Added Combat and pokemon relationship

---
+++ PokemonProject/database/migrations/2022_12_15_111549_create_combat_pokemon_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCombatPokemonTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('combat_pokemons', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('combat_id')->unsigned();
+            $table->bigInteger('pokemon_id')->unsigned();
+            $table->foreign('pokemon_id')
+                ->references('id')
+                ->on('pokemons')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+            $table->foreign('combat_id')
+                ->references('id')
+                ->on('combats')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+            $table->integer('health');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('combat_pokemon');
+    }
+}
